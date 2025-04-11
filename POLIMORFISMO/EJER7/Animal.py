09e7eeebb3781c125c6a828c82f0f83c20ff9cea
Maye199819 <suxomayerli@gmail.com>
2025-04-11 00:25:15 -0400

    Create Animal.py

---
+++ POLIMORFISMO/EJER7/Animal.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def hacerSonido(self):
+        print("Sonido generico de animal")
+
+    def moverse(self):
+        print("Movimiento generico de animal")
